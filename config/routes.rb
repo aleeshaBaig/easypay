f468a6c8dd3c952686cee39e0925fa0f79a4f496
[Aleesha] <[aleesha@pitb.gov.pk]>
2022-08-02 16:57:53 +0500

    Error Fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
-   root 'home#index'
-    resources :utility_bills do
+  resources :utility_bills do
       collection do
         get 'companies_details'
         get'search'
       end
     end
-   resources :bills do
+  resources :bills do
     collection do
       post "pay"
     end
    end
-
+ root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
