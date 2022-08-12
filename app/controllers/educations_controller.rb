77bf52c6cdc957648d31cad175e755864e334ffe
[Aleesha] <[aleesha@pitb.gov.pk]>
2022-08-12 14:19:33 +0500

    Tailwindcss is Added

--- app/controllers/educations_controller.rb
+++ app/controllers/educations_controller.rb
@@ -1,57 +1,52 @@
 class EducationsController < ApplicationController
-     before_action :authenticate_user!
-  
-     
-      def index
-        @personal_informations = Education
-        @contact_informations = ContactInformation.all
-         @emergency_contacts = EmergencyContact.all
-        @educations = Education.all
-  
-      end
-    
-      def show
-        @education= Education.find(params[:id])
-      end
-    
-      def new
-        @education= Education.new
-      end
-    
-      def create
-        @education = Education.new(education_params)
-         
-    
-        if @education.save
-          redirect_to @education
-        else
-          render :new, status: :unprocessable_entity
-        end
-      end
-      def destroy
-        @education = Education.find(params[:id])
-        @education.destroy
-    
-        redirect_to education_path, status: :see_other
-      end
-    
-      def edit
-        @education= Education.find(params[:id])
-      end
-    
-      def update
-        @education= Education.find(params[:id])
-    
-        if   @education.update(education_params)
-          redirect_to   personal_information_path 
-        else
-          render :edit, status: :unprocessable_entity
-        end
-      end
-    
-      private
-        def  education_params
-          params.require(:education).permit(:title, :major, :institute, :date, :user_id)
-        end
+before_action :authenticate_user!
+    def index
+      @educations = Education.where(user_id: current_user.id)
+
+    end
+
+    def show
+    @education= Education.find(params[:id])
+    end
+
+    def new
+    @education= Education.new
+    end
+
+    def create
+    @education = Education.new(education_params)
+
+
+    if @education.save
+    redirect_to @education
+    else
+    render :new, status: :unprocessable_entity
+    end
+    end
+    def destroy
+    @education = Education.find(params[:id])
+    @education.destroy
+
+    redirect_to education_path, status: :see_other
+    end
+
+    def edit
+    @education= Education.find(params[:id])
+    end
+
+    def update
+    @education= Education.find(params[:id])
+
+    if   @education.update(education_params)
+    redirect_to   personal_information_path 
+    else
+    render :edit, status: :unprocessable_entity
+    end
+    end
+
+    private
+    def  education_params
+    params.require(:education).permit(:title, :major, :institute, :date, :user_id)
+    end
     end
 
