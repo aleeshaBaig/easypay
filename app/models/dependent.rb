ef5fd05bdf20a4cbf005bfac30022d18b0c4c73f
[Aleesha] <[aleesha@pitb.gov.pk]>
2022-08-18 15:32:39 +0500

    Add validation in missing forms

--- app/models/dependent.rb
+++ app/models/dependent.rb
@@ -1,10 +1,10 @@
 class Dependent < ApplicationRecord
-  validates :name, presence: true
-  validates :relation, presence: true
-  validates :phone, presence: true
+  validates :name, presence: true,uniqueness: { scope: :user_id }
+  validates :relation, presence: true,uniqueness: { scope: :user_id }
+  validates :phone, presence: true,uniqueness: { scope: :user_id }
   validates :date_of_birth, presence: true
   validates :address, presence: true
-  validates :cnic, presence: true
+  validates :cnic, presence: true,uniqueness: { scope: :user_id }
   validates :nationality, presence: true
  
 belongs_to :user
--- app/models/emergency_contact.rb
+++ app/models/emergency_contact.rb
@@ -1,5 +1,9 @@
 class EmergencyContact < ApplicationRecord
-  validates :emergency_contact_name, presence: true
+  validates :emergency_contact_name, presence: true, uniqueness: { scope: :user_id }
+  validates  :emergency_contact_mobile, presence: true,uniqueness: { scope: :user_id }
+  validates  :emergency_contact_relation, presence: true
+  validates  :permanent_address, presence: true
+  validates  :residence_address, presence: true
 
   belongs_to :user
 end
