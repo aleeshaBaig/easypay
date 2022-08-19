556a2659290760bdf9a2a8bfacdf16c86658dda7
[Aleesha] <[aleesha@pitb.gov.pk]>
2022-08-19 15:00:57 +0500

    Changed the layout of Essaypay

--- app/controllers/emergency_contacts_controller.rb
+++ app/controllers/emergency_contacts_controller.rb
@@ -15,25 +15,31 @@ before_action :authenticate_user!
     end
     def create
       @emergency_contact  = EmergencyContact.new(emergency_contact_params)
+      respond_to do |format|
       if  @emergency_contact.save
-        redirect_to personal_informations_path
+        format.html { redirect_to personal_informations_url(@emergency_contact), notice: "Education was successfully created." }
+        format.json { render :show, status: :created, location: @emergency_contact }
       else
-        render :new, status: :unprocessable_entity
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @emergency_contact.errors, status: :unprocessable_entity }
       end
     end
+  end
     def  edit
       @emergency_contact  = EmergencyContact.find(params[:id])    
     end
     def  update
       @emergency_contact  = EmergencyContact.find(params[:id])
+      respond_to do |format|
       if  @emergency_contact.update(emergency_contact_params)
-        redirect_to  personal_informations_path
+        format.html { redirect_to personal_informations_url(@emergency_contact), notice: "Education was successfully created." }
+        format.json { render :show, status: :created, location: @emergency_contact }
       else
-        render :edit, status: :unprocessable_entity
-  
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @emergency_contact.errors, status: :unprocessable_entity }
       end
-     
-    end
+   end
+end
     private
     def emergency_contact_params
       params.require(:emergency_contact).permit(:emergency_contact_name, :emergency_contact_relation, :emergency_contact_mobile, :residence_address, :permanent_address, :user_id)
